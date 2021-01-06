51d109dc9cefa726ac1bc301ad0b213eb217f2d6
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-06 15:07:07 -0600

    jQuery

---
+++ JQuery/index.js
@@ -0,0 +1,7 @@
+/*  When placing the jQuery script on the head, this ready implementation is required
+
+$(document).ready(function (){
+   $("h1").css("color","red"); //$ jQuery
+});*/
+
+$("h1").css("color","red"); //$ jQuery
