ceff668425ac8c79780d1c91ddb7f1c29c36e62c
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-07 13:45:06 -0600

    jQuery, managing: css, classes, attributes and event listeners

--- JQuery/index.js
+++ JQuery/index.js
@@ -4,4 +4,36 @@ $(document).ready(function (){
    $("h1").css("color","red"); //$ jQuery
 });*/
 
-$("h1").css("color","red"); //$ jQuery
+//css
+$("h1").css("color","blue"); //$ jQuery
+
+$("button");
+//classes
+$("h1").addClass("big-title margin");
+$("h1").removeClass("big-title");
+$("h1").hasClass("margin");
+
+//atributes
+$("h1").text("Bye");
+$("button").text("Don't click me");
+$("button").html("<em>Hey</em>")
+//console.log($("a").attr("href", "https://www.yahoo.com"));
+
+// event listener
+$("h1").click(function (){
+    $("h1").css("color","purple");
+})
+
+//add a property to multiple html elements
+$("button").click(function (){
+    $("h1").css("color","purple");
+})
+
+//keypress
+$(document).keypress(function (){
+    $("h1").text(event.key);
+})
+//more events
+$("h1").on("mouseover",function (){
+    $("h1").css("color", "green");
+})
