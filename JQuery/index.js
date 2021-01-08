f59b07e459b4a03f8064153cc4a6bedd31083cfe
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-07 20:12:49 -0600

    jQuery, managing: css, classes, attributes and event listeners

--- JQuery/index.js
+++ JQuery/index.js
@@ -37,3 +37,9 @@ $(document).keypress(function (){
 $("h1").on("mouseover",function (){
     $("h1").css("color", "green");
 })
+
+// adding and removing html elements with jQuery
+$("h1").before("<button>before</button>");
+$("h1").after("<button>after</button>");
+$("h1").prepend("<button>prepend</button>");
+$("h1").append("<button>append</button>");
