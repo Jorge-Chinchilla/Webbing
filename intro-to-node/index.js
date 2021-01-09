2380eb213460d563fa73153e135d2bade1f9680c
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-09 13:14:54 -0600

    Node.js, node package manager npm

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,4 +1,11 @@
 //jshint esversion:6
 
+//internal node package
 const fs = require('fs');
 fs.copyFileSync("file1.txt", "file2.txt");
+
+//external npm
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+console.log("Superhero fight:")
+console.log(superheroes.random()+" vs "+supervillains.random());
