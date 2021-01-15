a3c55ac974827557e1a1baf0937742e6c699a1f1
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-14 21:18:18 -0600

    WeatherProject, Parsing to JSON the request data and extracting specific information.

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -9,6 +9,15 @@ app.get("/", function (req, res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=4123bb3f7d209d0c0578c9629baf2747";
     https.get(url, function (response){ //request to the api using the native https node package
         console.log(response.statusCode);
+
+        response.on("data", function (data){
+            const weatherData = JSON.parse(data); //converting the data in to a js object
+            console.log(weatherData);
+            const object = JSON.stringify(weatherData); //converts the object into a string
+            const temp = weatherData.main.temp; // getting a specific data from t he nested object
+            const description = weatherData.weather[0].description;
+            console.log(temp + " " + description);
+        });
     });
     res.send("Working");
 });
