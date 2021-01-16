3da55d31996f75fefc2623bf0caa4e5ba47b8785
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-15 21:01:21 -0600

    WeatherProject, Rendering the API data to the website

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -6,7 +6,7 @@ const ora = require('ora');
 const spinner = ora('Loading server').start();
 
 app.get("/", function (req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=4123bb3f7d209d0c0578c9629baf2747";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=4123bb3f7d209d0c0578c9629baf2747&units=metric";
     https.get(url, function (response){ //request to the api using the native https node package
         console.log(response.statusCode);
 
@@ -16,10 +16,16 @@ app.get("/", function (req, res){
             const object = JSON.stringify(weatherData); //converts the object into a string
             const temp = weatherData.main.temp; // getting a specific data from t he nested object
             const description = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
             console.log(temp + " " + description);
+            res.write("<p>The weather is currently "+ weatherData.weather[0].description+"</p><br>");
+            res.write("<h1>"+temp + " degrees in London</h1>");
+            console.log(imageURL);
+            res.write("<img src="+imageURL+">");
+            res.send();
         });
     });
-    res.send("Working");
 });
 
 
