861cbaba7a98796cb150338228f33e774475c142
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-15 21:19:09 -0600

    WeatherProject, making queries to search for different cities weather

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const ora = require('ora');
 const spinner = ora('Loading server').start();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=4123bb3f7d209d0c0578c9629baf2747&units=metric";
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res){
+
+    const query = req.body.cityName;
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=4123bb3f7d209d0c0578c9629baf2747&units=" + unit;
     https.get(url, function (response){ //request to the api using the native https node package
         console.log(response.statusCode);
 
@@ -20,15 +29,14 @@ app.get("/", function (req, res){
             const imageURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
             console.log(temp + " " + description);
             res.write("<p>The weather is currently "+ weatherData.weather[0].description+"</p><br>");
-            res.write("<h1>"+temp + " degrees in London</h1>");
+            res.write("<h1>"+temp + " degrees in "+query+"</h1>");
             console.log(imageURL);
             res.write("<img src="+imageURL+">");
             res.send();
         });
     });
+
 });
-
-
 
 app.listen(3000, function (){
     setTimeout(() => {
