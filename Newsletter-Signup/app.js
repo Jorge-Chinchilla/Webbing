236b8f2dbdf8e1f55f43ed09e45680089d35d3d3
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-18 14:44:31 -0600

    Newsletter, app is ready for Heroku deployment.

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -8,7 +8,7 @@ const client = require("@mailchimp/mailchimp_marketing");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 const spinner = ora('Loading server').start();
-client.setConfig({apiKey: "3c23636b2a6901d8005888eb28b3d378-us7",  server: "us7",});
+client.setConfig({apiKey: "enter-key",  server: "enter-server",});
 
 app.use(express.static("public")); // we add the folder that we want to keep as static for the resources
 
@@ -21,7 +21,7 @@ app.post("/", function (req, res){
     const lastName = req.body.lName;
     const email = req.body.email;
     const run = async () => {
-        const response = await client.lists.addListMember("77077d757d", {
+        const response = await client.lists.addListMember("enter-list", {
             email_address: email,
             status: "subscribed",
             merge_fields: {
@@ -34,39 +34,36 @@ app.post("/", function (req, res){
     run();
     res.sendFile(__dirname+"/success.html")
 
-/*    const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields:{
-                    FNAME: firstName,
-                    LNAME: lastName
+    /*    const data = {
+            members: [
+                {
+                    email_address: email,
+                    status: "subscribed",
+                    merge_fields:{
+                        FNAME: firstName,
+                        LNAME: lastName
+                    }
                 }
-            }
-        ]
-    };
-    const jsonData = JSON.stringify(data);
-    const urlMailChimp = "https://us7.api.mailchimp.com/3.0/lists/77077d757d/members";
-    const options = {
-        method: "POST",
-        auth: "jorgech1:3c23636b2a6901d8005888eb28b3d378-us7"
-    }{
-    const request = https.request(urlMailChimp, options, function (){
-        response.on("data", function (data){
-            console.log(JSON.parse(data));
+            ]
+        };
+        const jsonData = JSON.stringify(data);
+        const urlMailChimp = "https://us7.api.mailchimp.com/3.0/lists/77077d757d/members";
+        const options = {
+            method: "POST",
+            auth: "jorgech1:3c23636b2a6901d8005888eb28b3d378-us7"
+        }{
+        const request = https.request(urlMailChimp, options, function (){
+            response.on("data", function (data){
+                console.log(JSON.parse(data));
+            })
         })
-    })
-    request.write(jsonData);
-    request.end();*/
+        request.write(jsonData);
+        request.end();*/
 })
 
-app.listen(process.env.PORT || 3000, function (){ //process.env.PORT for heroku
+app.listen(process.env.PORT || 5000, function (){ //process.env.PORT for heroku
     setTimeout(() => {
         spinner.color = 'blue';
         spinner.text = 'Server running';
     }, 1000);
 })
-
-//mail chimp api key 3c23636b2a6901d8005888eb28b3d378-us7
-//list id 77077d757d    
