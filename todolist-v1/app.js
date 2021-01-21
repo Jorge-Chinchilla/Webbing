fcd5d33aed0e8228a65dfd3393a98a909cd27dcd
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-20 19:36:17 -0600

    ejs, passing data to from the website to the server, and redirecting the data on the website

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,36 +3,24 @@ const bodyParser = require("body-parser");
 const ora = require('ora');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 const spinner = ora('Loading server').start();
 
+//global
+var items = ["Buy food", "Play siege", "Sleep"];
+
 app.get("/", function (req, res){
     var today = new Date();
-    var day = "";
-    switch (today.getDay()){
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-    }
-    res.render('list', {kindOfDay: day});
+    var options = {weekday: 'long', day: 'numeric', month: 'long'}
+    var day= today.toLocaleDateString("en-US", options);
+    res.render('list', {kindOfDay: day, newListItems: items});
+})
+
+app.post("/", function (req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
 })
 
 app.listen(3000, function (){
