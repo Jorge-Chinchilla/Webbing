3294290bb8a4cf0a51d8df48205ad91e1fb6d2ff
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-01-27 17:32:45 -0600

    TodoList, handling layouts and templating with ejs

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,22 +5,43 @@ const ora = require('ora');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 const spinner = ora('Loading server').start();
 
 //global
-var items = ["Buy food", "Play siege", "Sleep"];
+let items = ["Buy food", "Play siege", "Sleep"];
+let workItems = [];
 
 app.get("/", function (req, res){
-    var today = new Date();
-    var options = {weekday: 'long', day: 'numeric', month: 'long'}
-    var day= today.toLocaleDateString("en-US", options);
-    res.render('list', {kindOfDay: day, newListItems: items});
+    let today = new Date();
+    let options = {weekday: 'long', day: 'numeric', month: 'long'}
+    let day= today.toLocaleDateString("en-US", options);
+    res.render('list', {listTitle: day, newListItems: items});
 })
 
 app.post("/", function (req, res){
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
+    let item = req.body.newItem;
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect('/work');
+    }else{
+        items.push(item);
+        res.redirect('/');
+    }
+})
+
+app.get("/work", function (req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function (req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
+})
+
+app.get("/about", function (req, res){
+    res.render("about");
 })
 
 app.listen(3000, function (){
