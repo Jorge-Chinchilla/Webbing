258a5e72841441ec18aed66ec480a1d640c438f2
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2021-04-24 18:19:55 -0600

    Este es un nuevo commit

---
+++ Secrets lvl 5/app.js
@@ -0,0 +1,112 @@
+//jshint esversion:6
+require('dotenv').config(); //needs to be on the very top
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs')
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(session({
+    secret: 'Our little secret',
+    resave: false,
+    saveUninitialized:  false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.set('useCreateIndex', true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+//encryption
+const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+////------------ '/' route
+app.route('/')
+    .get( function (req, res){
+        res.render('home');
+        }
+    );
+
+////------------ '/login route
+app.route('/login')
+    .get( function (req, res){
+            res.render('login');
+        }
+    )
+    .post(function (req, res) {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        })
+        req.login(user, function (err) {
+            if(err){
+                console.log(err);
+            }else{
+                passport.authenticate('local')(req, res, function () {
+                    res.redirect('/secrets');
+                })
+            }
+        })
+    });
+
+////------------ '/register' route
+app.route('/register')
+    .get( function (req, res){
+            res.render('register');
+        }
+    )
+    .post(function (req, res) {
+        User.register({username: req.body.username}, req.body.password, function (err, user) {
+            if(err){
+                console.log(err);
+                res.redirect('/register');
+            }else {
+                passport.authenticate('local')(req, res, function(){
+                    res.redirect('/secrets');
+                });
+            }
+        })
+    });
+
+app.route('/logout')
+    .get( function (req, res) {
+            req.logout();
+            res.redirect('/');
+    });
+
+app.route('/secrets')
+    .get(function (req, res) {
+        if(req.isAuthenticated()){
+            res.render('secrets');
+        }else{
+            res.redirect('/login');
+        }
+    });
+
+
+app.listen(3000, function () {
+    console.log('Server running');
+});
